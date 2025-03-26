8baba51405816aaa7049a8ee6554d842f7a87aa9
matsufriends <pikapikan0526@gmail.com>
2025-03-27 03:54:13 +0900

    MornEase/MornTweenの再構成

---
+++ MornEaseType.cs
@@ -0,0 +1,37 @@
+﻿namespace MornEase
+{
+    public enum MornEaseType
+    {
+        EaseInSine,
+        EaseOutSine,
+        EaseInOutSine,
+        EaseInQuad,
+        EaseOutQuad,
+        EaseInOutQuad,
+        EaseInCubic,
+        EaseOutCubic,
+        EaseInOutCubic,
+        EaseInQuart,
+        EaseOutQuart,
+        EaseInOutQuart,
+        EaseInQuint,
+        EaseOutQuint,
+        EaseInOutQuint,
+        EaseInExpo,
+        EaseOutExpo,
+        EaseInOutExpo,
+        EaseInCirc,
+        EaseOutCirc,
+        EaseInOutCirc,
+        EaseInBack,
+        EaseOutBack,
+        EaseInOutBack,
+        EaseInElastic,
+        EaseOutElastic,
+        EaseInOutElastic,
+        EaseInBounce,
+        EaseOutBounce,
+        EaseInOutBounce,
+        Linear,
+    }
+}
